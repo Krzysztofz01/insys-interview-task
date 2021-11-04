a60ef4499ba7a4616214120c222fd3a547928f50
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-11-04 11:52:37 +0100

    Exception middleware

---
+++ src/MovieLibrary.Api/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MovieLibrary.Api.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next ??
+                throw new ArgumentNullException(nameof(next));
+
+            _logger = logger ??
+                throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failure on: {ex}");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+            httpContext.Response.ContentType = "application/json";
+
+            httpContext.Response.StatusCode = ex switch
+            {
+                InvalidOperationException _ => (int)HttpStatusCode.BadRequest,
+                ArgumentException _ => (int)HttpStatusCode.BadRequest,
+                _ => (int)HttpStatusCode.InternalServerError,
+            };
+
+            var serializedResponse = JsonSerializer.Serialize(new
+            {
+                StatusCode = httpContext.Response.StatusCode,
+                Message = ex.Message
+            });
+            
+            await httpContext.Response.WriteAsync(serializedResponse);
+        }
+    }
+}
