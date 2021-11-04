19de0b1353b6d8bbcfaccf790df31b624958b4d9
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-11-05 00:27:16 +0100

    Unit testes for repositories

---
+++ src/MovieLibrary.Test/Repositories/CategoryRepositoryTests.cs
@@ -0,0 +1,168 @@
+﻿using Moq;
+using MovieLibrary.Data.Contracts;
+using MovieLibrary.Data.Entities;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MovieLibrary.Test.Repositories
+{
+    public class CategoryRepositoryTests
+    {
+        [Fact]
+        public async void GetCategoriesAsyncShouldReturnCategories()
+        {
+            // Arrange
+            var categoryInMemoryDatabase = new Collection<Category>
+            {
+                new Category { Id = 1, Name = "Category one" },
+                new Category { Id = 2, Name = "Category two"},
+                new Category { Id = 3, Name = "Category three" }
+            };
+
+            var repository = new Mock<ICategoryRepository>();
+
+            repository.Setup(r => r.GetCategoriesAsync())
+                .Returns(() => Task.FromResult(categoryInMemoryDatabase.AsEnumerable()));
+
+            var expectedCount = 3;
+
+            // Act
+            var actual = await repository.Object.GetCategoriesAsync();
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.Equal(expectedCount, actual.Count());
+        }
+
+        [Fact]
+        public async void GetCategoriesAsyncWithPredicateShouldReturnCategories()
+        {
+            // Arrange
+            var categoryInMemoryDatabase = new Collection<Category>
+            {
+                new Category { Id = 1, Name = "Category one" },
+                new Category { Id = 2, Name = "Category two"},
+                new Category { Id = 3, Name = "Category three" }
+            };
+
+            var repository = new Mock<ICategoryRepository>();
+
+            repository.Setup(r => r.GetCategoriesAsync(It.IsAny<Expression<Func<Category, bool>>>()))
+                .Returns((Expression<Func<Category, bool>> p) => Task.FromResult(categoryInMemoryDatabase.Where(p.Compile())));
+
+            var expectedId = 1;
+            var expectedCount = 1;
+
+            // Act
+            var actual = await repository.Object.GetCategoriesAsync(e => e.Id == expectedId);
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.Equal(expectedCount, actual.Count());
+            Assert.Equal(expectedId, actual.Single().Id);
+        }
+
+        [Fact]
+        public async void GetCategoryByIdAsyncShouldReturnCategory()
+        {
+            // Arrange
+            var categoryInMemoryDatabase = new Collection<Category>
+            {
+                new Category { Id = 1, Name = "Category one" },
+                new Category { Id = 2, Name = "Category two"},
+                new Category { Id = 3, Name = "Category three" }
+            };
+
+            var repository = new Mock<ICategoryRepository>();
+
+            repository.Setup(r => r.GetCategoryByIdAsync(It.IsAny<int>()))
+                .Returns((int i) => Task.FromResult(categoryInMemoryDatabase.SingleOrDefault(b => b.Id == i)));
+
+            var expectedName = "Category one";
+            var expectedId = 1;
+
+            // Act
+            var actual = await repository.Object.GetCategoryByIdAsync(expectedId);
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.Equal(expectedId, actual.Id);
+            Assert.Equal(expectedName, actual.Name);
+        }
+
+        [Fact]
+        public async void InsertCategoryAsyncShouldAddNewCategory()
+        {
+            // Arrange
+            var categoryInMemoryDatabase = new Collection<Category>
+            {
+                new Category { Id = 1, Name = "Category one" },
+                new Category { Id = 2, Name = "Category two"},
+                new Category { Id = 3, Name = "Category three" }
+            };
+
+            var repository = new Mock<ICategoryRepository>();
+
+            repository.Setup(r => r.GetCategoryByIdAsync(It.IsAny<int>()))
+                .Returns((int i) => Task.FromResult(categoryInMemoryDatabase.SingleOrDefault(b => b.Id == i)));
+
+            repository.Setup(r => r.InsertCategoryAsync(It.IsAny<Category>()))
+                .Returns((Category c) =>
+                {
+                    categoryInMemoryDatabase.Add(c);
+
+                    return Task.CompletedTask;
+                });
+
+            var expectedName = "Category four";
+            var expectedId = 4;
+
+            // Act
+            await repository.Object.InsertCategoryAsync(new Category { Id = expectedId, Name = expectedName });
+
+            var actual = await repository.Object.GetCategoryByIdAsync(expectedId);
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.Equal(expectedId, actual.Id);
+            Assert.Equal(expectedName, actual.Name);
+        }
+
+        [Fact]
+        public async void DeleteCategoryShouldRemoveCateogry()
+        {
+            // Arrange
+            var categoryInMemoryDatabase = new Collection<Category>
+            {
+                new Category { Id = 1, Name = "Category one" },
+                new Category { Id = 2, Name = "Category two"},
+                new Category { Id = 3, Name = "Category three" }
+            };
+
+            var repository = new Mock<ICategoryRepository>();
+
+            repository.Setup(r => r.GetCategoryByIdAsync(It.IsAny<int>()))
+                .Returns((int i) => Task.FromResult(categoryInMemoryDatabase.SingleOrDefault(b => b.Id == i)));
+
+            repository.Setup(r => r.DeleteCategory(It.IsAny<Category>()))
+                .Callback((Category c) => categoryInMemoryDatabase.Remove(c))
+                .Verifiable();
+
+            var expectedCount = 2;
+            var expectedId = 1;
+
+            // Act
+            var category = await repository.Object.GetCategoryByIdAsync(expectedId);
+
+            repository.Object.DeleteCategory(category);
+
+            // Assert
+            Assert.Null(categoryInMemoryDatabase.SingleOrDefault(m => m.Id == expectedId));
+            Assert.Equal(expectedCount, categoryInMemoryDatabase.Count);
+        }
+    }
+}
---
+++ src/MovieLibrary.Test/Repositories/MovieRepositoryTests.cs
@@ -0,0 +1,168 @@
+﻿using Moq;
+using MovieLibrary.Data.Contracts;
+using MovieLibrary.Data.Entities;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MovieLibrary.Test.Repositories
+{
+    public class MovieRepositoryTests
+    {
+        [Fact]
+        public async void GetMoviesAsyncShouldReturnMovies()
+        {
+            // Arrange
+            var movieInMemoryDatabase = new Collection<Movie>
+            {
+                new Movie { Id = 1, Title = "Movie one" },
+                new Movie { Id = 2, Title = "Movie two"},
+                new Movie { Id = 3, Title = "Movie three" }
+            };
+
+            var repository = new Mock<IMovieRepository>();
+
+            repository.Setup(r => r.GetMoviesAsync())
+                .Returns(() => Task.FromResult(movieInMemoryDatabase.AsEnumerable()));
+
+            var expectedCount = 3;
+
+            // Act
+            var actual = await repository.Object.GetMoviesAsync();
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.Equal(expectedCount, actual.Count());
+        }
+
+        [Fact]
+        public async void GetMoviesAsyncWithPredicateShouldReturnMovies()
+        {
+            // Arrange
+            var movieInMemoryDatabase = new Collection<Movie>
+            {
+                new Movie { Id = 1, Title = "Movie one" },
+                new Movie { Id = 2, Title = "Movie two"},
+                new Movie { Id = 3, Title = "Movie three" }
+            };
+
+            var repository = new Mock<IMovieRepository>();
+
+            repository.Setup(r => r.GetMoviesAsync(It.IsAny<Expression<Func<Movie, bool>>>()))
+                .Returns((Expression<Func<Movie, bool>> p) => Task.FromResult(movieInMemoryDatabase.Where(p.Compile())));
+
+            var expectedId = 1;
+            var expectedCount = 1;
+
+            // Act
+            var actual = await repository.Object.GetMoviesAsync(e => e.Id == expectedId);
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.Equal(expectedCount, actual.Count());
+            Assert.Equal(expectedId, actual.Single().Id);
+        }
+
+        [Fact]
+        public async void GetMovieByIdAsyncShouldReturnMovie()
+        {
+            // Arrange
+            var movieInMemoryDatabase = new Collection<Movie>
+            {
+                new Movie { Id = 1, Title = "Movie one" },
+                new Movie { Id = 2, Title = "Movie two"},
+                new Movie { Id = 3, Title = "Movie three" }
+            };
+
+            var repository = new Mock<IMovieRepository>();
+
+            repository.Setup(r => r.GetMovieByIdAsync(It.IsAny<int>()))
+                .Returns((int i) => Task.FromResult(movieInMemoryDatabase.SingleOrDefault(b => b.Id == i)));
+
+            var expectedTitle = "Movie one";
+            var expectedId = 1;
+
+            // Act
+            var actual = await repository.Object.GetMovieByIdAsync(expectedId);
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.Equal(expectedId, actual.Id);
+            Assert.Equal(expectedTitle, actual.Title);
+        }
+
+        [Fact]
+        public async void InsertMovieAsyncShouldAddNewMovie()
+        {
+            // Arrange
+            var movieInMemoryDatabase = new Collection<Movie>
+            {
+                new Movie { Id = 1, Title = "Movie one" },
+                new Movie { Id = 2, Title = "Movie two"},
+                new Movie { Id = 3, Title = "Movie three" }
+            };
+
+            var repository = new Mock<IMovieRepository>();
+
+            repository.Setup(r => r.GetMovieByIdAsync(It.IsAny<int>()))
+                .Returns((int i) => Task.FromResult(movieInMemoryDatabase.SingleOrDefault(b => b.Id == i)));
+
+            repository.Setup(r => r.InsertMovieAsync(It.IsAny<Movie>()))
+                .Returns((Movie m) =>
+                {
+                    movieInMemoryDatabase.Add(m);
+
+                    return Task.CompletedTask;
+                });
+
+            var expectedTitle = "Movie four";
+            var expectedId = 4;
+
+            // Act
+            await repository.Object.InsertMovieAsync(new Movie { Id = expectedId, Title = expectedTitle });
+
+            var actual = await repository.Object.GetMovieByIdAsync(expectedId);
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.Equal(expectedId, actual.Id);
+            Assert.Equal(expectedTitle, actual.Title);
+        }
+
+        [Fact]
+        public async void DeleteMovieShouldRemoveMovie()
+        {
+            // Arrange
+            var movieInMemoryDatabase = new Collection<Movie>
+            {
+                new Movie { Id = 1, Title = "Movie one" },
+                new Movie { Id = 2, Title = "Movie two"},
+                new Movie { Id = 3, Title = "Movie three" }
+            };
+
+            var repository = new Mock<IMovieRepository>();
+
+            repository.Setup(r => r.GetMovieByIdAsync(It.IsAny<int>()))
+                .Returns((int i) => Task.FromResult(movieInMemoryDatabase.SingleOrDefault(b => b.Id == i)));
+
+            repository.Setup(r => r.DeleteMovie(It.IsAny<Movie>()))
+                .Callback((Movie m) => movieInMemoryDatabase.Remove(m))
+                .Verifiable();
+
+            var expectedCount = 2;
+            var expectedId = 1;
+
+            // Act
+            var movie = await repository.Object.GetMovieByIdAsync(expectedId);
+
+            repository.Object.DeleteMovie(movie);
+
+            // Assert
+            Assert.Null(movieInMemoryDatabase.SingleOrDefault(m => m.Id == expectedId));
+            Assert.Equal(expectedCount, movieInMemoryDatabase.Count);
+        }
+    }
+}
